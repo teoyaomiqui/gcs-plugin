d220fedb5a1312e650a5e6aea076a2544c18af40
root <root@nailgun.test.domain.local>
2016-07-14 08:24:35 +0000

    backup_configs working now

--- deployment_scripts/gcs_cinder_conf.pp
+++ deployment_scripts/gcs_cinder_conf.pp
@@ -1,7 +1,16 @@
 notice('MODULAR: gcs_cinder_config.pp')
-
+$services = ['cinder-backup']
 $gcs_config_values                    = hiera_hash('gcs')
 
 class { '::config::backup' :
   gcs_config_values                   => $gcs_config_values
 }
+
+service { $services:
+  ensure => running;
+}
+
+Cinder_config <||> ~> Service[$services]
+
+
+
